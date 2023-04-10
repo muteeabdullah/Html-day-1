e1e209d73a685a40011e5dfc51df410d88f50652
Mutee Abdullah <mutee.abdullah.02@gmail.com>
2023-04-10 23:10:21 +0500

    1day

---
+++ script.js
@@ -0,0 +1,37 @@
+var button = document.getElementById('btn');
+var para = document.getElementById('main1');
+var j = 0;
+para.innerHTML = j;
+button.addEventListener('mouseover', function () {
+    j = j + 1;
+
+    para.innerHTML = j;
+
+});
+var input = document.getElementById('input');
+var image = document.getElementById('img');
+var image1 = document.getElementById('img1');
+
+
+function displayImage(event) {
+    var reader = new FileReader();
+    reader.onload = function () {
+        image.src = reader.result;
+    }
+    reader.readAsDataURL(event.target.files[0]);
+}
+
+function displayImage1(event) {
+    var reader = new FileReader();
+    reader.onload = function () {
+        image1.src = reader.result;
+    }
+    reader.readAsDataURL(event.target.files[1]);
+}
+
+var comment = document.getElementById('comment');
+var commentBox = document.getElementById('showComment');
+function showComment() {
+    var commentValue = comment.value;
+    commentBox.innerHTML = commentValue;
+}
